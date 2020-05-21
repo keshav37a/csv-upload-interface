6d5ea275eddfd544a4a66c5f974eff658a532f8a
Keshav Mathur <keshav37a@gmail.com>
2020-05-21 19:03:16 +0530

    set up ejs-views and sass

--- controllers/uploads_controller.js
+++ controllers/uploads_controller.js
@@ -1,4 +1,4 @@
 module.exports.home = (req, res)=>{
     console.log(`home in uploads_controller called`);
-    return res.send(`<h1>home in uploads_controller called</h1>`);
+    return res.render('home.ejs');
 }
--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express')
 const port = 8000;
 const app = new express();
+const fs = require('fs');
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+const sassMiddleware = require('node-sass-middleware');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}))
 
 app.use('/', require('./routes/index'));
 
